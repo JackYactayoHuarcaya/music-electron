c12a8ccf2d74a0e08ebd79e7e22eff2c7c619d4f
jack <jack-0204@hotmail.com>
2025-08-29 17:30:20 -0500

    feat: read

--- dist-electron/main.js
+++ dist-electron/main.js
@@ -1,9 +1,9 @@
 import "electron";
 import "node:url";
 import "node:path";
-import { Y as m, Z as p, X as t } from "./main-Zq7DLSnH.js";
+import { Y, Z, X } from "./main-DDfDAg4y.js";
 export {
-  m as MAIN_DIST,
-  p as RENDERER_DIST,
-  t as VITE_DEV_SERVER_URL
+  Y as MAIN_DIST,
+  Z as RENDERER_DIST,
+  X as VITE_DEV_SERVER_URL
 };
